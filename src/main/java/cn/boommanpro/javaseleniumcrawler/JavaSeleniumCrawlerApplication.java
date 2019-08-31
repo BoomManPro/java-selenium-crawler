8e48d2187547afb98b250422b9469f7cff3f3c87
BoomManPro <boommanpro@gmail.com>
2019-08-31 17:03:09 +0800

    Initial Project

---
+++ src/main/java/cn/boommanpro/javaseleniumcrawler/JavaSeleniumCrawlerApplication.java
@@ -0,0 +1,13 @@
+package cn.boommanpro.javaseleniumcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeleniumCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSeleniumCrawlerApplication.class, args);
+    }
+
+}
